b754a0960089598fa98283d3f81cc0ddb2986a65
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-09-24 11:22:08 +0100

    Finished portfolio template

--- static/js/main.js
+++ static/js/main.js
@@ -16,7 +16,7 @@ const projects = [
         image: "static/images/placeholder.jpg",
         title: "Ecommerce Store",
         desc: 'This is an online store developed in the Django framework that allows users to place orders, create their own accounts or shop as guests.',
-        technologies: ['Python', 'JavaScript', 'HTML', 'CSS', 'Stripe'],
+        technologies: ['Python', 'JavaScript', 'HTML/CSS', 'Stripe'],
         githubLink: 'https://github.com/drakata27/django-online-store'
     },
     {
@@ -24,7 +24,7 @@ const projects = [
         image: "static/images/placeholder.jpg",
         title: "Booklog",
         desc: 'Booklog is a CRUD web app that allows users to log their favourite books and write description about the book. This applciation was developed in the Django framework',
-        technologies: ['Python','HTML', 'CSS', 'Bootstrap'],
+        technologies: ['Python','HTML/CSS', 'Bootstrap'],
         githubLink: 'https://github.com/drakata27/booklog'
     },
     {
@@ -56,15 +56,40 @@ const projects = [
         image: "static/images/placeholder.jpg",
         title: "Chat App",
         desc: 'NOTE: STILL IN DEVELOPMENT. Chat application developed in the Django framework that allows users to communicate in real time using Django Channels',
-        technologies: ['Python', 'JavaScript', 'HTML', 'CSS', 'Bootstrap'],
+        technologies: ['Python', 'JavaScript', 'HTML/CSS', 'Bootstrap'],
         githubLink: '#'
     },
 ]
 
-const projectsDiv = document.querySelector('#projects')
-let projectsContent = ''
+// const projectsDiv = document.querySelector('#projects')
+// let projectsContent = ''
+
+// projects.forEach(project => {
+//     const projectContent = `
+//         <div class="project">
+//             <img src="${project.image}" class="project-img" />
+//             <div class="des">
+//                 <div class="title">
+//                     <h3>${project.title}</h3>
+//                     <p>${project.desc}</p>
+//                     <h4>Technologies</h4>
+//                     <p>${project.technologies}</p>
+//                 </div>
+//             </div>
+//             <a class="btn btn-outline-dark me-2 mx-1 btn-primary" href="${project.githubLink}">Source Code</a>
+//         </div>
+//         `;
+//     projectsContent += projectContent;
+
+//     projectsDiv.innerHTML = projectsContent;
+// });
+
+const projectsDiv = document.querySelector('#projects');
+let projectsContent = '';
 
 projects.forEach(project => {
+    const technologies = project.technologies.map(tech => `<span class="technology">${tech}</span>`).join(' ');
+
     const projectContent = `
         <div class="project">
             <img src="${project.image}" class="project-img" />
@@ -73,13 +98,13 @@ projects.forEach(project => {
                     <h3>${project.title}</h3>
                     <p>${project.desc}</p>
                     <h4>Technologies</h4>
-                    <p>${project.technologies}</p>
+                    <div class="technologies">${technologies}</div>
                 </div>
             </div>
             <a class="btn btn-outline-dark me-2 mx-1 btn-primary" href="${project.githubLink}">Source Code</a>
         </div>
         `;
     projectsContent += projectContent;
-
-    projectsDiv.innerHTML = projectsContent;
 });
+
+projectsDiv.innerHTML = projectsContent;
